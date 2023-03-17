2d3ebc42482ce05e4d9abb4a0e855e6c01fe381b
S-BEE <bshukurah96@gmail>
2023-03-17 13:05:20 +0100

    signup

---
+++ signup.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form>
+
+<label for="name">Name:</label>
+<input type="text" id="" name="" placeholder="" value=""><br><br>
+
+
+<label for="contact">Address:</label>
+<input type="text" id="contact" name="" placeholder="" value=""><br><br>
+
+<label for="phone">Phone Number:</label>
+<input type="number" id="phone" name="" placeholder="" value=""><br><br>
+
+
+<label for="email">Email: </label>
+<input type="email" id="email" name="" placeholder="" value=""><br><br>
+
+
+<select name="state" id="state">
+    <option value="selector"> select state</option>
+    <option value="lagos">lagos</option>
+    <option value="Abuja">Abuja</option>
+    <option value="kano">kano</option>
+</select>
+    
+
+<p> Select Your Track</p>
+<input type="radio" id="" value="frontend" name="">
+<label for="">Frontend</label>
+
+<input type="radio" id="" value="backend" name="">
+<label for="">Backend</label>
+
+
+<input type="radio" id="" value="" name="">
+<label for="">Full-stack</label><br><br>
+
+<textarea name="about" id="msg" cols="30" rows="10">Tell us about you</textarea>
+
+
+<button>Submit</button>
+
+</form>
+
+</body>
+</html>
